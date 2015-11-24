241f0b8e32f987810b3961f2d6954ccd635d60f3
Jaime Lennox <jl6912@imperial.ac.uk>
2015-11-24 15:15:46 +0000

    Incremental loop unwinding, refactor z3 finding, and use program arguments to enable loop unwinding.

--- tool/VCGenerator.java
+++ tool/VCGenerator.java
@@ -10,6 +10,9 @@ public class VCGenerator {
 	private ProcedureDeclContext proc;
 	private List<SimpleCParser.VarDeclContext> globals;
 	private Map<String, ProcedureSummarisation> summarisationMap;
+	private final int UNROLLING_DEPTH;
+	private final boolean UNROLL_LOOPS;
+
 	
 	public VCGenerator(ProcedureDeclContext proc,
 					   List<SimpleCParser.VarDeclContext> globals,
@@ -18,19 +21,39 @@ public class VCGenerator {
 		this.globals = globals;
 		this.summarisationMap = summarisationMap;
 
+		UNROLL_LOOPS = false;
+		UNROLLING_DEPTH = 0;
+
 		// The static variables in the Variables class mean that we use the
 		// wrong ids/declarations for global variables, so we need to reset these
 		// across procedures.
 		Variables.refresh();
 	}
-	
+
+	public VCGenerator(ProcedureDeclContext proc,
+					   List<SimpleCParser.VarDeclContext> globals,
+					   Map<String, ProcedureSummarisation> summarisationMap,
+					   int unrollingDepth) {
+		this.proc = proc;
+		this.globals = globals;
+		this.summarisationMap = summarisationMap;
+
+		UNROLL_LOOPS = true;
+		UNROLLING_DEPTH = unrollingDepth;
+
+		// The static variables in the Variables class mean that we use the
+		// wrong ids/declarations for global variables, so we need to reset these
+		// across procedures.
+		Variables.refresh();
+	}
+
 	public StringBuilder generateVC() {
 		StringBuilder result = new StringBuilder("(set-logic QF_BV)\n");
 		result.append("(set-option :produce-models true)\n");
 		result.append("(define-fun tobv32 ((p Bool)) (_ BitVec 32) (ite p (_ bv1 32) (_ bv0 32)))\n");
 		result.append("(define-fun tobool ((p (_ BitVec 32))) Bool (ite (= p (_ bv0 32)) false true))\n");
 
-		SimpleCSMTVisitor visitor = new SimpleCSMTVisitor(summarisationMap);
+		SimpleCSMTVisitor visitor = UNROLL_LOOPS ? new SimpleCSMTVisitor(summarisationMap, UNROLLING_DEPTH) : new SimpleCSMTVisitor(summarisationMap);
 
 		for( SimpleCParser.VarDeclContext ctx : globals ) {
 			result.append(ctx.accept(visitor));
